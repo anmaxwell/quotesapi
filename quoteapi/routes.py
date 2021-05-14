c4d633912b0e627d25ddb8629d5a223dd079966d
anmaxwell <aniamaxwell@gmail.com>
2021-05-14 09:41:12 +0100

    more work for post endpoint

--- quoteapi/routes.py
+++ quoteapi/routes.py
@@ -19,9 +19,10 @@ def quote():
     #return str(text.quote)
     return jsonify(index=quote_no,quote=str(text.quote))
 
-@app.route("/api/addquote")
+@app.route("/api/addquote", methods=['GET', 'POST'])
 def add_quote():
 
-    newquote = Quote(quote=request.json['quote'])
-    db.session.add(newquote)
-    db.session.commit()
+    newquote = request.json['quote']
+    #newquote = Quote(quote=request.json['quote'])
+    #db.session.add(newquote)
+    #db.session.commit()
